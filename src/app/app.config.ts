e5a704aa5a230e93161329befdcf93f7b4d10ebe
Marco Ammann <marco-ammann@outlook.com>
2025-02-24 00:35:36 +0100

    hero with split overlay

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -7,6 +7,7 @@ import { provideRouter } from '@angular/router';
 import {provideAnimationsAsync} from'@angular/platform-browser/animations/async';
 import { routes } from './app.routes';
 import { provideServiceWorker } from '@angular/service-worker';
+import { provideHttpClient } from '@angular/common/http';
 
 export const appConfig: ApplicationConfig = {
   providers: [
@@ -17,5 +18,6 @@ export const appConfig: ApplicationConfig = {
       registrationStrategy: 'registerWhenStable:30000',
     }),
     provideAnimationsAsync(),
+    provideHttpClient(),
   ],
 };
--- src/app/components/home/hero/hero.component.ts
+++ src/app/components/home/hero/hero.component.ts
@@ -1,36 +1,48 @@
-import { Component, AfterViewInit } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, AfterViewInit, Host, HostListener } from '@angular/core';
 import { Router } from '@angular/router';
 import Typed from 'typed.js';
 
 @Component({
   selector: 'app-hero',
   standalone: true,
+  imports: [CommonModule],
   templateUrl: './hero.component.html',
   styleUrls: ['./hero.component.scss']
 })
 export class HeroComponent implements AfterViewInit {
+  scrollPosition = 0;
   constructor(private router: Router) {}
 
   ngAfterViewInit(): void {
-    if (window.innerWidth >= 900) {
-      const options = {
-        stringsElement: '#typed-strings',
-        typeSpeed: 60,
-        backSpeed: 30,
-        backDelay: 900,
-        loop: false,
-        showCursor: true,
-      };
-      new Typed('#typed-text', options);
-    } else {
-      const typedElem = document.getElementById('typed-text');
-      if (typedElem) {
-        typedElem.innerText = 'Kreativität entfalten';
-      }
-    }
+    // if (window.innerWidth >= 900) {
+    //   const options = {
+    //     stringsElement: '#typed-strings',
+    //     typeSpeed: 60,
+    //     backSpeed: 30,
+    //     backDelay: 900,
+    //     loop: false,
+    //     showCursor: true,
+    //   };
+    //   new Typed('#typed-text', options);
+    // } else {
+    //   const typedElem = document.getElementById('typed-text');
+    //   if (typedElem) {
+    //     typedElem.innerText = 'Kreativität entfalten';
+    //   }
+    // }
+  }
+
+  @HostListener('window:scroll', ['$event'])
+  onScroll(event: any): void {
+    this.scrollPosition = window.pageYOffset;
   }
 
   navigateToWorkshops(): void {
     this.router.navigate(['/workshops']);
+  }
+
+  exploreGallery(): void {
+    this.router.navigate(['/gallery']);
   }
 }
